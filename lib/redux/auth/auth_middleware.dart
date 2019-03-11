44bf7359a4c0c8b5ea782f7dfe612f2a7b8df2a1
Hillel Coren <hillelcoren@gmail.com>
2019-03-11 19:44:31 +0200

    Login/logout

--- lib/redux/auth/auth_middleware.dart
+++ lib/redux/auth/auth_middleware.dart
@@ -35,9 +35,7 @@ void _saveAuthLocal(ArtistEntity artist) async {
 
 Middleware<AppState> _createLoginInit() {
   return (Store<AppState> store, dynamic action, NextDispatcher next) {
-    
     Navigator.of(action.context).pushReplacementNamed(LoginScreenBuilder.route);
-    //Navigator.of(action.context).pushReplacementNamed(MainScreen.route);
 
     next(action);
   };
