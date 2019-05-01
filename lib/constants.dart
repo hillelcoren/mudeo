7c57033ae944c2c1340888b370da4c0e0e769847
Hillel Coren <hillelcoren@gmail.com>
2019-05-01 18:15:38 +0300

    Update version

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-const String kAppVersion = '0.0.9+9';
+const String kAppVersion = '0.0.10+10';
 const String kTermsOfServiceURL = 'https://mudeo.app/terms';
 const String kPrivacyPolicyURL = 'https://mudeo.app/privacy';
 const String kRedditURL = 'https://www.reddit.com/r/mudeo';
