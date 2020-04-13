99da3bb992087e10502bdf17bf63b59e2aa8edf9
Hillel Coren <hillelcoren@gmail.com>
2020-04-13 19:24:58 +0300

    IAP bug fixes

--- lib/ui/auth/upgrade_dialog.dart
+++ lib/ui/auth/upgrade_dialog.dart
@@ -190,9 +190,10 @@ class _UpgradeDialogState extends State<UpgradeDialog> {
       title: Column(
         children: <Widget>[
           Text(localization.privateStorage),
+          SizedBox(height: 8),
           if (Platform.isIOS)
             Padding(
-              padding: const EdgeInsets.only(top: 8, bottom: 4),
+              padding: const EdgeInsets.only(bottom: 8),
               child: Text(
                 'Payment will be charged to iTunes Account at confirmation of purchase. Subscription automatically renews unless auto-renew is turned off at least 24-hours before the end of the current period. Account will be charged for renewal within 24-hours prior to the end of the current period, and identify the cost of the renewal. Subscriptions may be managed by the user and auto-renewal may be turned off by going to the user\'s Account Settings after purchase.',
                 style: TextStyle(fontSize: 12, color: Colors.grey),
