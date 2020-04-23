887bec6afaf965f30910e18842b6ab7c25ce503b
Simon Lightfoot <simon@devangels.london>
2020-04-23 20:17:52 +0100

    Add pose painter

--- lib/ui/song/track_score.dart
+++ lib/ui/song/track_score.dart
@@ -1,8 +1,9 @@
 import 'dart:convert';
 import 'dart:io';
-import 'package:http/http.dart' as http;
+
 import 'package:document_analysis/document_analysis.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:mudeo/constants.dart';
 import 'package:mudeo/data/models/song_model.dart';
 import 'package:mudeo/utils/localization.dart';
@@ -22,6 +23,7 @@ class _TrackScoreState extends State<TrackScore> {
   bool _isProcessing = false;
   double _distance;
   List<int> _frameTimes;
+  List<double> _frameScores;
   List<String> _origPaths;
   List<String> _copyPaths;
 
@@ -49,13 +51,16 @@ class _TrackScoreState extends State<TrackScore> {
       return;
     }
 
-
+    _frameScores = [];
     int count = 0;
     for (int i = 0; i < song.duration; i += kRecognitionFrameSpeed) {
       final value = _calculateFrameScore(count);
       if (value != null) {
         _distance += value;
         countParts++;
+        _frameScores.add(value);
+      }else{
+        _frameScores.add(0);
       }
       count++;
     }
@@ -156,6 +161,12 @@ class _TrackScoreState extends State<TrackScore> {
   }
 
   @override
+  void reassemble() {
+    super.reassemble();
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
     final localization = AppLocalization.of(context);
     final song = widget.song;
@@ -165,64 +176,164 @@ class _TrackScoreState extends State<TrackScore> {
     print('## origData: $origData');
 
     return AlertDialog(
-        contentPadding: const EdgeInsets.all(16),
-        actions: <Widget>[
-          if (_frameTimes == null)
-            FlatButton(
-              child: Text(localization.showDetails.toUpperCase()),
-              onPressed: () {
-                _calculateDetails();
-              },
-            ),
+      contentPadding: const EdgeInsets.all(16),
+      actions: <Widget>[
+        if (_frameTimes == null)
           FlatButton(
-            child: Text(localization.close.toUpperCase()),
+            child: Text(localization.showDetails.toUpperCase()),
             onPressed: () {
-              Navigator.of(context).pop();
+              _calculateDetails();
             },
-          )
-        ],
-        content: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              if (_distance != null) ...[
-                Padding(
-                  padding: const EdgeInsets.all(15),
-                  child: Text('Your score is:'),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  '${(100 - (_distance * 100)).round()}%',
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-                SizedBox(height: 20),
-              ],
-              if (_isProcessing)
-                LinearProgressIndicator()
-              else if (_frameTimes != null)
-                for (int i = 0; i < _frameTimes.length; i++)
-                  Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Stack(
-                          children: <Widget>[
-                            Image.file(File(_origPaths[
-                                _frameTimes.indexOf(_frameTimes[i])])),
-                            Text(origData[i].toString()),
-                          ],
+          ),
+        FlatButton(
+          child: Text(localization.close.toUpperCase()),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        )
+      ],
+      content: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            if (_distance != null) ...[
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: Text('Your score is:'),
+              ),
+              SizedBox(height: 10),
+              Text(
+                '${(100 - (_distance * 100)).round()}%',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              SizedBox(height: 20),
+            ],
+            if (_isProcessing)
+              LinearProgressIndicator()
+            else if (_frameTimes != null)
+              for (int i = 0; i < _frameTimes.length; i++)
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: _PoseDisplay(
+                        index: i,
+                        frame: origData[i].cast<String, List<dynamic>>(),
+                        color: Colors.blue,
+                        child: Image.file(
+                          File(_origPaths[_frameTimes.indexOf(_frameTimes[i])]),
+                          color: Colors.black87,
+                          colorBlendMode: BlendMode.srcOver,
                         ),
                       ),
-                      Expanded(
-                          child: Stack(
-                        children: <Widget>[
-                          Image.file(File(
-                              _copyPaths[_frameTimes.indexOf(_frameTimes[i])])),
-                          Text(copyData[i].toString()),
-                        ],
-                      )),
-                    ],
-                  ),
-            ],
-          ),
-        ));
+                    ),
+                    Expanded(
+                      child: _PoseDisplay(
+                        index: i,
+                        frame: copyData[i].cast<String, List<dynamic>>(),
+                        color: Colors.red,
+                        child: Image.file(
+                          File(_copyPaths[_frameTimes.indexOf(_frameTimes[i])]),
+                          color: Colors.black87,
+                          colorBlendMode: BlendMode.srcOver,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _PoseDisplay extends StatelessWidget {
+  const _PoseDisplay({
+    Key key,
+    @required this.index,
+    @required this.frame,
+    @required this.color,
+    @required this.child,
+  }) : super(key: key);
+
+  final int index;
+  final Map<String, List<dynamic>> frame;
+  final Color color;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      foregroundPainter: _PosePainter(
+        index: index,
+        frame: frame,
+        color: color,
+      ),
+      child: child,
+    );
+  }
+}
+
+class _PosePainter extends CustomPainter {
+  _PosePainter({
+    @required this.index,
+    @required this.frame,
+    @required this.color,
+  });
+
+  final int index;
+  final Map<String, List<dynamic>> frame;
+  final Color color;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2.0
+      ..color = color;
+
+    final Paint circlePaint = Paint()
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 3.0
+      ..color = color;
+
+    Offset _extract(int key) {
+      final List<double> point = frame[key.toString()].cast<double>();
+      return Offset(point[0], point[1]);
+    }
+
+    _drawLine(canvas, size, _extract(kRecognitionPartLeftAnkle), _extract(kRecognitionPartLeftKnee), paint);
+    _drawLine(canvas, size, _extract(kRecognitionPartLeftKnee), _extract(kRecognitionPartLeftHip), paint);
+    _drawLine(canvas, size, _extract(kRecognitionPartLeftHip), _extract(kRecognitionPartLeftShoulder), paint);
+    _drawLine(canvas, size, _extract(kRecognitionPartLeftShoulder), _extract(kRecognitionPartLeftElbow), paint);
+    _drawLine(canvas, size, _extract(kRecognitionPartLeftElbow), _extract(kRecognitionPartLeftWrist), paint);
+    _drawLine(canvas, size, _extract(kRecognitionPartRightAnkle), _extract(kRecognitionPartRightKnee), paint);
+    _drawLine(canvas, size, _extract(kRecognitionPartRightKnee), _extract(kRecognitionPartRightHip), paint);
+    _drawLine(canvas, size, _extract(kRecognitionPartRightHip), _extract(kRecognitionPartRightShoulder), paint);
+    _drawLine(canvas, size, _extract(kRecognitionPartRightShoulder), _extract(kRecognitionPartRightElbow), paint);
+    _drawLine(canvas, size, _extract(kRecognitionPartRightElbow), _extract(kRecognitionPartRightWrist), paint);
+    _drawLine(canvas, size, _extract(kRecognitionPartLeftHip), _extract(kRecognitionPartRightHip), paint);
+    _drawLine(canvas, size, _extract(kRecognitionPartLeftShoulder), _extract(kRecognitionPartRightShoulder), paint);
+
+    for(final entry in frame.entries) {
+      final point = entry.value.cast<double>();
+      final dx = point[0] * size.width;
+      final dy = point[1] * size.height;
+      canvas.drawCircle(Offset(dx, dy), 1.0, circlePaint);
+    }
+  }
+
+  void _drawLine(Canvas canvas, Size size, Offset a, Offset b, Paint paint) {
+    if (a != null && b != null) {
+      canvas.drawLine(
+        a.scale(size.width, size.height),
+        b.scale(size.width, size.height),
+        paint,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(_PosePainter oldDelegate) {
+    return index != oldDelegate.index;
   }
 }
