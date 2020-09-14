f00cb4eef2abb999181ec0e7bd98bd284b4e4b51
Hillel Coren <hillelcoren@gmail.com>
2020-09-15 00:23:20 +0300

    Update version

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-const String kAppVersion = '0.0.46+46';
+const String kAppVersion = '0.0.47+47';
 const String kDeveloperURL = 'https://twitter.com/hillelcoren';
 const String kGitHubURL = 'https://github.com/hillelcoren/mudeo';
 
