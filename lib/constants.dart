2d5e423d0d25bfe717a968a8a272b259933c8b28
Hillel Coren <hillelcoren@gmail.com>
2021-06-17 17:37:05 +0300

    Disable desktop off web

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-const String kAppVersion = '0.0.53+53';
+const String kAppVersion = '0.0.54+54';
 const String kDeveloperURL = 'https://twitter.com/hillelcoren';
 const String kGitHubURL = 'https://github.com/hillelcoren/mudeo';
 
